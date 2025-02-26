b944a4ce2417cc174f192c3dda8b4f168802b331
MayaraBinsfeld <mayara.binsfeld@gmail.com>
2025-02-26 10:51:40 -0300

    Add files via upload

---
+++ ex4_helloname.py
@@ -0,0 +1,18 @@
+"""
+Programa: ex4_helloname
+Descrição: Programa para perguntar o nome do usuário, e cumprimentá-lo.
+Autor: Mayara Binsfeld
+Data: 26/02/2025
+Versão: 0.0.1
+
+"""
+# ALOCAÇÃO DE MEMÓRIA
+name = input("Qual seu nome?")
+
+# ENTRADA DE DADOS
+
+# PROCESSAMENTO DE DADOS
+
+# SAÍDA DE DADOS
+
+print(f"Hello, {name}")
